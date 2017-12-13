7e13e83b198de657928b8ba6751b6ce94d3eab56
risenanti <risenanti@gmail.com>
2017-12-13 14:46:02 -0600

    working on sliding window to display data

---
+++ kintechUart.srcs/sources_1/new/topDriver.vhd
@@ -0,0 +1,91 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity topDriver is
+  Port ( 
+        SYSCLK_300_P           : IN STD_LOGIC; --300MHZ CLK
+        SYSCLK_300_N           : IN STD_LOGIC; 
+        heartBeatLED           : OUT STD_LOGIC;
+        
+        USB_UART_RX_FPGA_TX_LS : OUT  STD_LOGIC
+  );
+end topDriver;
+
+architecture Behavioral of topDriver is
+
+component clk_wiz_0
+port
+ (-- Clock in ports
+  -- Clock out ports
+  clk_out1          : out    std_logic;
+  clk_in1           : in     std_logic
+ );
+end component;
+
+signal clk300MHZ : std_logic := '0';
+signal clk10MHZ  : std_logic := '0';
+
+component uartFtdi is
+
+	generic(
+		BitWidth 	  		   : integer := 32;
+		NumberOfBytes 		   : integer :=4
+	);
+    Port ( 
+		clk10MHZ  	  		   : IN std_logic; 
+		heartBeatLED  		   : OUT STD_LOGIC;
+      
+		DIN           		   : IN std_logic_vector(BitWidth downto 0);
+		USB_UART_RX_FPGA_TX_LS : OUT  STD_LOGIC
+    );
+end component;
+
+signal txData1 : std_logic_vector(7 downto 0) := "01000001"; --'A'
+signal txData2 : std_logic_vector(7 downto 0) := "01000010"; --'B'
+signal txData3 : std_logic_vector(7 downto 0) := "01000011"; --'C'
+signal txData4 : std_logic_vector(7 downto 0) := "01000100"; --'D'
+
+signal txDataSend : std_logic_vector (31 downto 0);
+
+begin
+
+    txDataSend <= txData1&txData2&txData3&txData4;
+
+    clk_buf :IBUFGDS
+        generic map(
+            IBUF_LOW_PWR => TRUE,
+            -- low power (TRUE) vs. performance (FALSE) setting for referenced I/O standards
+            IOSTANDARD => "LVDS"
+        )
+        port map (
+            O  => clk300MHZ, -- clock buffer output
+            I  => SYSCLK_300_P,    -- diff_p clock buffer input
+            IB => SYSCLK_300_N     -- diff_n clock buffer input
+        );
+        
+        clkMHZ : clk_wiz_0
+           port map ( 
+          -- Clock out ports  
+           clk_out1 => clk10MHZ,
+           -- Clock in ports
+           clk_in1 => clk300MHZ
+         );
+         
+       test1 : uartFTDI
+       generic map (BitWidth => 31, NumberOfBytes => 4)
+			port map(
+			clk10MHZ => clk10MHZ, heartBeatLED => heartBeatLED,
+			DIN => txDataSend,
+			USB_UART_RX_FPGA_TX_LS => USB_UART_RX_FPGA_TX_LS);
+			
+
+end Behavioral;
--- kintechUart.srcs/sources_1/new/uartFtdi.vhd
+++ kintechUart.srcs/sources_1/new/uartFtdi.vhd
@@ -11,34 +11,20 @@ library UNISIM;
 use UNISIM.VComponents.all;
 
 entity uartFtdi is
-  Port ( 
-      SYSCLK_300_P           : IN STD_LOGIC; --300MHZ CLK
-      SYSCLK_300_N           : IN STD_LOGIC; 
-      heartBeatLED           : OUT STD_LOGIC;
+	generic(
+		BitWidth 	  		   : integer := 32;
+		NumberOfBytes 		   : integer :=4
+	);
+    Port ( 
+		clk10MHZ  	  		   : IN std_logic; 
+		heartBeatLED  		   : OUT STD_LOGIC;
       
-      USB_UART_RX_FPGA_TX_LS : OUT  STD_LOGIC
-  );
+		DIN           		   : IN std_logic_vector(BitWidth downto 0);
+		USB_UART_RX_FPGA_TX_LS : OUT  STD_LOGIC
+    );
 end uartFtdi;
 
 architecture Behavioral of uartFtdi is
-
---signal count : integer := 0;
-signal count : unsigned(15 downto 0);
-signal count_next : unsigned(15 downto 0);
-constant one : unsigned(15 downto 0) := "0000000000000001";
-signal clk300MHZ : std_logic;
-signal clk10MHZ  : std_logic;
-signal blink : std_logic;
-signal blink_next : std_logic;
-
-component clk_wiz_0
-port
- (-- Clock in ports
-  -- Clock out ports
-  clk_out1          : out    std_logic;
-  clk_in1           : in     std_logic
- );
-end component;
 
 component mod_m_counter is
    generic(
@@ -68,36 +54,28 @@ component uart_tx is
    );
 end component;
 
+signal count : unsigned(15 downto 0);
+signal count_next : unsigned(15 downto 0);
+
+signal blink : std_logic;
+signal blink_next : std_logic;
+
 signal txStart : STD_LOGIC;
 signal txStart_next : std_logic;
 signal txDone  : STD_LOGIC;
-signal txDataBuffer : std_logic_vector(7 downto 0) := "01000001"; -- 'A'
+signal txDataBuffer : unsigned(7 downto 0) := "01000001"; -- 'A'
 signal baudRateTick: std_logic; --SETS BAUD RATE
 
+signal txShiftBuffer : unsigned (BitWidth downto 0);
+signal txDataShiftTemp : unsigned (BitWidth downto 0);
+signal lowerBounds : integer := 0;
+--signal upperBounds : integer := 7;
+
 begin
-    clk_buf :IBUFGDS
-        generic map(
-            IBUF_LOW_PWR => TRUE,
-            -- low power (TRUE) vs. performance (FALSE) setting for referenced I/O standards
-            IOSTANDARD => "LVDS"
-        )
-        port map (
-            O  => clk300MHZ, -- clock buffer output
-            I  => SYSCLK_300_P,    -- diff_p clock buffer input
-            IB => SYSCLK_300_N     -- diff_n clock buffer input
-        );
-        
-        clkMHZ : clk_wiz_0
-           port map ( 
-          -- Clock out ports  
-           clk_out1 => clk10MHZ,
-           -- Clock in ports
-           clk_in1 => clk300MHZ
-         );
         
         process(count, blink)
         begin
-         if (count > 60000) then --300000000 1 second
+         if (count > 60000) then --10000000 1 second
              blink_next   <= not blink;
              count_next <= to_unsigned(0,16);
              txStart_next <= not txStart;
@@ -105,28 +83,49 @@ begin
              count_next <=count+1;
              blink_next   <=  blink;
          end if;    
-        end process;
-        
-        
+        end process; 
         
         process(clk10MHZ)
             begin
+            
+            txDataShiftTemp <= shift_right(txShiftBuffer, lowerBounds);
+            txDataBuffer<= txDataShiftTemp(7 downto 0);
+                        
+            if(lowerBounds > BitWidth) then
+				lowerBounds <= 0;
+				end if;
+            
             if(rising_edge(clk10MHZ)) then
-               --count <= std_logic_vector (unsigned(count)+unsigned(one));
                count <= count_next;
                blink  <=  blink_next;
                txStart<=txStart_next;
                 end if;   
             end process;
-                       
-baudRateGenerator: mod_m_counter
-    generic map(N=>8, M=>62) --10mhz clk to get 115200 * 16 for oversampling
-    port map(clk=>clk10MHZ, reset=>'0', max_tick=>baudRateTick, q=>open);
             
-tansmitOut : uart_tx generic map(DBIT=>8, SB_TICK=>16)
-                 port map (clk=>clk10MHZ, reset => '0', 
-                          tx_start => txStart, s_tick=>baudRateTick,
-                          din => txDataBuffer,tx_done_tick => txDone, 
-                          tx => USB_UART_RX_FPGA_TX_LS); 
-heartBeatLED <= blink;
+        process(txDone)
+           begin
+            if(rising_edge(txDone)) then
+            	--txStart<='0';
+            	lowerBounds<=lowerBounds+7;
+            end if;
+            
+            if (lowerBounds>BitWidth) then
+                lowerBounds <= 0;
+            end if;  
+            end process;         
+        
+        txShiftBuffer <= unsigned(DIN);               
+        baudRateGenerator: mod_m_counter
+						
+						   generic map(N=>8, M=>62) 
+						   port map(clk=>clk10MHZ, 
+						   reset=>'0', max_tick=>baudRateTick, q=>open);
+            
+	    tansmitOut       : uart_tx 
+	                       generic map(DBIT=>8, SB_TICK=>16)
+                           port map (clk=>clk10MHZ, reset => '0', 
+						   tx_start => txStart, s_tick=>baudRateTick,
+						   din => std_logic_vector(txDataBuffer),tx_done_tick => txDone, 
+						   tx => USB_UART_RX_FPGA_TX_LS); 
+        heartBeatLED <= blink;
 end Behavioral;
